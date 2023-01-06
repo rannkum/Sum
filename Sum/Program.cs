c155400cd57a35e4a16173edd2883f46efcc08eb
rannkum <122127628+rannkum@users.noreply.github.com>
2023-01-06 15:06:37 -0700

    firstcheckin

---
+++ Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("Please enter the number: ");
+int n = int.Parse(Console.ReadLine());
+var sum = 0;
+while (n > 0)
+{
+    sum += n % 10; 
+    n = n / 10;
+}
+Console.WriteLine("Sum of the digits is: " + sum);
